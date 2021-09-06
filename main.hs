0673b5f84d4a49584af1f25fe31c222d97348cdf
Tjark Weber <tjark.weber@gmx.de>
2021-09-06 16:15:59 +0200

    Add intersection exercise

--- main.hs
+++ main.hs
@@ -28,6 +28,19 @@ prop_Exercise2 n = n < 1 || Assignment2.sumSquareDiff n == sumSquareDiff n
 
 -- Exercise 3
 
+_ = Assignment2.inter :: [Integer] -> [Integer] -> [Integer]
+
+inter :: [Integer] -> [Integer] -> [Integer]
+inter = Data.List.intersect
+
+prop_Exercise3 s1 s2 =
+  let s1' = Data.List.nub s1
+      s2' = Data.List.nub s2
+  in
+    Data.List.sort (Assignment2.inter s1' s2') == Data.List.sort (inter s1' s2')
+
+-- Exercise 4
+
 _ = Assignment2.isMatch :: String -> String -> Bool
 
 isMatch :: String -> String -> Bool
@@ -37,18 +50,18 @@ isMatch (c : s) (d : p)   = c==d && isMatch s p
 isMatch "" "" = True
 isMatch _ _ = False
 
-newtype Exercise3String = Exercise3String String
+newtype Exercise4String = Exercise4String String
 
-instance Show Exercise3String where
-  show (Exercise3String s) = show s
+instance Show Exercise4String where
+  show (Exercise4String s) = show s
 
-instance Arbitrary Exercise3String where
-  arbitrary = liftM Exercise3String $ frequency
+instance Arbitrary Exercise4String where
+  arbitrary = liftM Exercise4String $ frequency
                                         [ (1, arbitrary)
                                         , (9, listOf $ elements ['a','b','c','*','?'])
                                         ]
 
-prop_Exercise3 (Exercise3String s) (Exercise3String p) = Assignment2.isMatch s p == isMatch s p
+prop_Exercise4 (Exercise4String s) (Exercise4String p) = Assignment2.isMatch s p == isMatch s p
 
 -- main
 
@@ -57,4 +70,6 @@ main = do
   putStrLn "Exercise 2:"
   quickCheck prop_Exercise2
   putStrLn "Exercise 3:"
-  quickCheck (withMaxSuccess 1000 prop_Exercise3)
+  quickCheck prop_Exercise3
+  putStrLn "Exercise 4:"
+  quickCheck (withMaxSuccess 1000 prop_Exercise4)
