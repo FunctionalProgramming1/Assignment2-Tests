77481c1e0683ba71cab155b35faedb3779b09545
Tjark Weber <tjark.weber@gmx.de>
2023-09-11 21:42:59 +0200

    Added exercise on variants

--- main.hs
+++ main.hs
@@ -20,28 +20,32 @@ import qualified Data.List
 
 -- Exercise 2
 
+prop_Exercise2 = not Assignment2.answer21 && Assignment2.answer22 && not Assignment2.answer23 && Assignment2.answer24 && not Assignment2.answer25
+
+-- Exercise 3
+
 _ = Assignment2.sumSquareDiff :: Integer -> Integer
 
 sumSquareDiff :: Integer -> Integer
 sumSquareDiff n =
   (sum [1..n])^2 - sum [ x^2 | x <- [1..n] ]
 
-prop_Exercise2 n = n < 1 || Assignment2.sumSquareDiff n == sumSquareDiff n
+prop_Exercise3 n = n < 1 || Assignment2.sumSquareDiff n == sumSquareDiff n
 
--- Exercise 3
+-- Exercise 4
 
 _ = Assignment2.inter :: [Integer] -> [Integer] -> [Integer]
 
 inter :: [Integer] -> [Integer] -> [Integer]
 inter = Data.List.intersect
 
-prop_Exercise3 s1 s2 =
+prop_Exercise4 s1 s2 =
   let s1' = Data.List.nub s1
       s2' = Data.List.nub s2
   in
     Data.List.sort (Assignment2.inter s1' s2') == Data.List.sort (inter s1' s2')
 
--- Exercise 4
+-- Exercise 5
 
 _ = Assignment2.isMatch :: String -> String -> Bool
 
@@ -52,18 +56,18 @@ isMatch (c : s) (d : p)   = c==d && isMatch s p
 isMatch "" "" = True
 isMatch _ _ = False
 
-newtype Exercise4String = Exercise4String String
+newtype Exercise5String = Exercise5String String
 
-instance Show Exercise4String where
-  show (Exercise4String s) = show s
+instance Show Exercise5String where
+  show (Exercise5String s) = show s
 
-instance Arbitrary Exercise4String where
-  arbitrary = liftM Exercise4String $ frequency
+instance Arbitrary Exercise5String where
+  arbitrary = liftM Exercise5String $ frequency
                                         [ (1, arbitrary)
                                         , (9, listOf $ elements ['a','b','c','*','?'])
                                         ]
 
-prop_Exercise4 (Exercise4String s) (Exercise4String p) = Assignment2.isMatch s p == isMatch s p
+prop_Exercise5 (Exercise5String s) (Exercise5String p) = Assignment2.isMatch s p == isMatch s p
 
 -- main
 
@@ -74,4 +78,6 @@ main = do
   putStrLn "Exercise 3:"
   quickCheck prop_Exercise3
   putStrLn "Exercise 4:"
-  quickCheck (withMaxSuccess 1000 prop_Exercise4)
+  quickCheck prop_Exercise4
+  putStrLn "Exercise 5:"
+  quickCheck (withMaxSuccess 1000 prop_Exercise5)
